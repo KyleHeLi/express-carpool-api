51a1b0ba139bc7019da9dd359650f31adbc0a5ae
kyleli <kyle_li@trendmicro.com>
2021-06-18 16:12:09 -0400

    init commit

---
+++ src/api/register.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  const alreayExistsUser = await User.findOne({ where: { email } }).catch(
+    (err) => {
+      console.log("Error: ", err);
+    }
+  );
+
+  if (alreayExistsUser) {
+    return res.status(409).json({ message: "User with email already exists!" });
+  }
+
+  const newUser = new User({ fullName, email, password });
+  const saveUser = await newUser.save().catch((err) => {
+    console.log("Error: ", err);
+    res.json({ error: "Could not register user at the moment!" });
+  });
+
+  if (saveUser) {
+    res.json({ message: "Thanks for registering!" });
+  }
+});
+
+module.exports = router;
